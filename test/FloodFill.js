d900adc0826c320e2f09b556bbe68412ec197896
Benjamin Van Treese <vantreeseba@gmail.com>
2018-05-26 03:31:33 -0500

    Increase test coverage

--- test/FloodFill.js
+++ test/FloodFill.js
@@ -4,7 +4,7 @@ const Map = require('../Map');
 
 module.exports = {
   'FloodFill': {
-    'should return all neighbors with same value': () => {
+    'should return all neighbors with same value and connected': () => {
       const map = new Map(4, 4);
       map.set(0, 0, 1);
       map.set(1, 0, 1);
@@ -12,6 +12,15 @@ module.exports = {
       let flooded = FloodFill(map, 0, 0, 1);
 
       assert.equal(2, flooded.length);
+    },
+    'should return all neighbors with same value': () => {
+      const map = new Map(4, 4);
+      map.set(0, 0, 1);
+      map.set(1, 0, 1);
+      map.set(0, 1, 1);
+      let flooded = FloodFill(map, 0, 0, 1);
+
+      assert.equal(3, flooded.length);
     },
   }
 };
